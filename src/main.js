ece75ff49c6fa6c8a1eb2dce1bb96660c1c638a9
Eric Fei <guanwha@gmail.com>
2019-08-22 04:53:48 +0800

    Installed vue-axios
    axios 0.19.0
    vue-axios 2.1.4

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App';
 import stores from '../src/stores';
 import router from '../src/routers';
 import './styles/all.scss';
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 /* eslint-disable no-new */
 new Vue({
